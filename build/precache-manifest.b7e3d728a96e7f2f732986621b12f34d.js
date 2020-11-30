e130b5eeeb73b1a68db35441b79fce611f313784
Nathan Villegas <nvillegasx@gmail.com>
2020-11-29 18:08:49 -0800

    github pages first build

---
+++ build/precache-manifest.b7e3d728a96e7f2f732986621b12f34d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde359631018e4928eec1bfff3c54f99",
+    "url": "/WhatYearGame/index.html"
+  },
+  {
+    "revision": "068361ca6573cda30c32",
+    "url": "/WhatYearGame/static/css/2.14f3cd7d.chunk.css"
+  },
+  {
+    "revision": "4d7dc3d062d8c69b850a",
+    "url": "/WhatYearGame/static/css/main.3379bc10.chunk.css"
+  },
+  {
+    "revision": "068361ca6573cda30c32",
+    "url": "/WhatYearGame/static/js/2.907dd486.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/WhatYearGame/static/js/2.907dd486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d7dc3d062d8c69b850a",
+    "url": "/WhatYearGame/static/js/main.6c314ee8.chunk.js"
+  },
+  {
+    "revision": "f8ce0ca4ef79b670bb3e",
+    "url": "/WhatYearGame/static/js/runtime-main.ec468798.js"
+  }
+]);
